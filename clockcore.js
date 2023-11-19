d2bbf0c70bf88e1d92f20fc7b3b33cb1d410d022
csharpython <121863991+csharpython@users.noreply.github.com>
2023-11-19 17:40:22 +0900

    Timezone
    タイムゾーンを表示するようにしました。

--- clockcore.js
+++ clockcore.js
@@ -1,4 +1,6 @@
 var clockes;
+const TIME_ZONE = -(new Date('August 19, 1975 0:00:00 GMT+00:00').getTimezoneOffset())/60;
+document.getElementById("timezone").innerHTML = "Timezone : GMT + "+TIME_ZONE+"hours";
 function clockUpdate(){
 	clockes = new Date().toLocaleString();
 	document.getElementById("time").innerHTML = clockes;
